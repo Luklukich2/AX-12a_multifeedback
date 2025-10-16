8ada5846a1bf8dea3acfd191bd0cab907a589b21
Luklukich2 <luukilev@gmail.com>
2025-10-16 17:55:19 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include <Dynamixel.h>
+#include <argviz.h>
+
+#define DXL_BAUDRATE    1000000
+#define DXL_SERIAL      dxlSerial
+#define DXL_PROTOCOL    1
+#define DXL_DIR_PIN     37
+
+// HardwareSerial dxlSerial(0);    
+
+const uint8_t DXL_ID1 = 1;
+const uint8_t DXL_ID2 = 2;
+const uint8_t DXL_ID3 = 3;
+const uint8_t DXL_ID4 = 4;
+const uint8_t DXL_ID5 = 5;
+
+short Position;
+short Voltage;
+short Temperature;
+int servo_id = 0;
+
+uint8_t counter = 0;
+String motorModel;
+
+int find_Pos(int servo_id)
+{
+  Position = Dynamixel.readPosition(servo_id);
+
+  return Position;
+}
+
+int find_Volt(int servo_id)
+{
+  Voltage = Dynamixel.readVoltage(servo_id);
+
+  return Voltage;
+}
+
+int find_Temp(int servo_id)
+{
+  Temperature = Dynamixel.readTemperature(servo_id);
+
+  return Temperature;
+}
+
+void find_Model(int servo_id)
+{
+  motorModel = Dynamixel.mapMotorModel((Dynamixel.readModel(servo_id)));
+}
+
+SCREEN(DX1, {
+         ROW("Position[]: %d", find_Pos(counter))
+         ROW("Voltage[]: %d", find_Volt(counter))
+         ROW("Temperature[]: %d", find_Temp(counter))
+         ROW("Model[]: %s", motorModel.c_str())
+         CLICK_ROW([](CLICK_STATE state)
+         {
+        switch(state)
+        {
+        case CLICK_LEFT:
+            counter--;
+            break;
+        case CLICK_RIGHT:
+            counter++;
+            break;
+        case CLICK_DOWN:
+            counter = 0;
+            break;
+        default:
+            break;
+        } }, "Servo_id %3u", counter);
+       });
+
+void setup() {
+
+  // dxlSerial.begin(DXL_BAUDRATE, SERIAL_8N1, RX, TX);  // Define your RX and TX pins "Feather S3 44-RX 43-TX"
+  Serial.begin(115200);                               // initialize serial communication at 115200 bps, Enable CDC on Boot
+
+  Dynamixel.begin(&Serial2, DXL_BAUDRATE, DXL_DIR_PIN, DXL_PROTOCOL);
+  delay(1000);
+
+  argviz_init(Serial);
+  argviz_registerScreen(0, DX1);
+  argviz_start();
+}
+
+void loop() {
+
+  // Position = Dynamixel.readPosition(DXL_ID1);
+  // Voltage = Dynamixel.readVoltage(DXL_ID1);
+  // Temperature = Dynamixel.readTemperature(DXL_ID1);
+  // motorModel = Dynamixel.mapMotorModel((Dynamixel.readModel(DXL_ID1)));
+
+  // Serial.print(motorModel); 
+  // Serial.print(" -> Position: ");Serial.print((Position*360) / 1023);
+  // Serial.print("° Temperature: ");Serial.print(Temperature);
+  // Serial.print("°C Voltage: ");Serial.print(Voltage/10);Serial.println("V.");
+
+  // delay(100);
+}
